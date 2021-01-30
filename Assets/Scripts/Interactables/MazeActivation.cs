19e98283b7e6680b91b51ba8b314bf2a278ce7c6
JorisotB <jorisotb@gmail.com>
2021-01-31 00:37:29 +0100

    Maze activation

---
+++ Assets/Scripts/Interactables/MazeActivation.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MazeActivation : MonoBehaviour
+{
+    public GameObject maze;
+    public GameObject corridor;
+    public GameObject timer;
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.CompareTag("Player")) {
+            corridor.SetActive(false);
+            maze.SetActive(true);
+            timer.SetActive(true);
+        }
+    }
+}
